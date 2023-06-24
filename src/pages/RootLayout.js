c92bf5d29ccbdd7128295c85948a11d922db1ea4
Jagrut Sharma <101259156+jagrut-sharma@users.noreply.github.com>
2023-06-24 22:18:03 +0530

    side bar

--- src/pages/RootLayout.js
+++ src/pages/RootLayout.js
@@ -1,17 +1,19 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Heading from "./Heading";
 import SideNavBar from "../components/SideNavBar";
 import RightBar from "../components/RightBar";
 
 export default function RootLayout() {
+  const location = useLocation();
+
   return (
     <div className="grid relative min-h-screen grid-rows-rootLayout font-Libre">
       <Heading />
       <div className="grid grid-cols-rootColLayout pt-4 justify-center relative">
         <SideNavBar />
         <Outlet />
-        <RightBar />
+        {location.pathname === "/" ? <RightBar /> : <div></div>}
       </div>
     </div>
   );
